c5ad35d60fcef0b5b4d186b52659d77a06b88d81
Adrien Boulaire <adr.boulaire@gmail.com>
2023-06-26 09:34:56 +0200

    Correction de l'affichage chargement parcours

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="text-center">
-    <div class="spinner-border" role="status">
-      <span> Chargement ...</span>
+  <div class="row justify-content-center align-items-center">
+    <div class="col text-center">
+      <div class="spinner-border" role="status"></div>
+      <div>Chargement...</div>
     </div>
   </div>
 </template>
